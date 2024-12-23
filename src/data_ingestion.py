270828903bf68a0726159a2f86cd2a73322337a1
sohailsheikh09 <smsheikh09@gmail.com>
2024-12-23 15:42:19 +0530

    new update

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -24,7 +24,7 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     try:
         df.drop(columns=['tweet_id'], inplace=True)
         final_df = df[df['sentiment'].isin(['happiness', 'sadness'])]
-        final_df['sentiment'].replace({'happiness': 1, 'sadness': 0}, inplace=True)
+        final_df['sentiment'].replace({'happiness': 1, 'sadness': 0})
         return final_df
     except KeyError as e:
         print(f"Error: Missing column {e} in the dataframe.")
