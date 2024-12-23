e9c00411d7519e591243ac10c3f844bf8d6ea2a5
sohailsheikh09 <smsheikh09@gmail.com>
2024-12-23 08:22:24 +0530

    Project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="sohail",
+    author_email="sohailsheikh09@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
